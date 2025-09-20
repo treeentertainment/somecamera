3021f69a4e13b0037e4c0a08327a295642320a00
channel101 <202932523+channel101@users.noreply.github.com>
2025-09-20 16:26:00 +0900

    init

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,5 @@
-import { QuartzConfig } from "./quartz/quartz/cfg"
-import * as Plugin from "./quartz/quartz/plugins"
-
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
 /**
  * Quartz 4 Configuration
  *
