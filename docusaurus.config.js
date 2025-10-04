4076ced33cabb78766c837863f805d3a2077b161
M <treeentertainment@outlook.kr>
2025-10-04 17:02:09 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,34 +1,59 @@
-// docusaurus.config.js
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'SomeCamera',
-  tagline: 'SomeCamera',
+  tagline: 'SomeCamera Documentation',
   url: 'https://camera.treeentertainment.tech',
   baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
   favicon: 'favicon.ico',
-  organizationName: 'treeentertainment', // Usually GitHub org/user name.
-  projectName: 'SomeCamera', // Usually repo name.
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['kr', 'en'],
+    localeConfigs: {
+      kr: { label: '한국어' },
+      en: { label: 'English' },
+    },
+  },
+
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/treeentertainment/somecamera/edit/main/docs',
+          path: 'docs',
+          routeBasePath: 'docs',
+          sidebarPath: ({ locale }) => {
+            switch (locale) {
+              case 'en':
+              default:
+                return require.resolve('./sidebars/sidebars_en.js');
+              case 'kr':
+                return require.resolve('./sidebars/sidebars_kr.js');
+            }
+          },
+          editUrl:
+            'https://github.com/treeentertainment/SomeCamera/edit/main/docs',
         },
       },
     ],
   ],
-  i18n: {
-  defaultLocale: 'en',          // 기본 언어
-  locales: ['kr', 'en'],        // 지원 언어
-  localeConfigs: {
-    kr: { label: '한국어' },
-    en: { label: 'English' },
+
+  themeConfig: {
+    navbar: {
+      title: 'SomeCamera',
+      items: [
+        { to: 'docs/intro', label: 'Docs', position: 'left' },
+        { type: 'localeDropdown', position: 'right' },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        { label 'github', href: 'https://github.com/treeentertainment/somecamera' },
+        { label: 'docs', to: 'docs/intro' },
+      ],
+    },
   },
-},
 };
 
 module.exports = config;
