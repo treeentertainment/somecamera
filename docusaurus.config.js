36196d424ea1a732b487e6a967e41157e78cf56b
M <treeentertainment@outlook.kr>
2025-10-05 11:22:10 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
     ],
     // Plugin for client redirects
     [
-      'docusaurus-plugin-client-redirects',
+      '@docusaurus/plugin-client-redirects',
       {
         redirects: [
           {
