7781adb0dbda70e2d0cd736faa49e413c1b840e3
M <treeentertainment@outlook.kr>
2025-10-06 11:29:24 +0900

    Update navbar and footer links in docusaurus.config.js

--- pages/config/docusaurus.config.js
+++ pages/config/docusaurus.config.js
@@ -84,7 +84,8 @@ export default {
     navbar: {
       title: 'SomeCamera',
       items: [
-        { to: './download/', label: 'Download', position: 'left' },
+        { href: '/github', label: 'GitHub', position: 'right' },
+        { href: '/download', label: 'Download', position: 'right' },
         { to: '/en/intro', label: 'Docs (EN)', position: 'left' },
         { to: '/kr/intro', label: 'Docs (KR)', position: 'left' },
       ],
@@ -92,8 +93,8 @@ export default {
     footer: {
       style: 'dark',
       links: [
-        { label: 'GitHub', to: './github' },
-        { label: 'Download' , to: './download/' },
+        { label: 'GitHub', href: '/github' },
+        { label: 'Download' , href: '/download' },
         { label: 'Docs (EN)', to: '/en/intro' },
         { label: 'Docs (KR)', to: '/kr/intro' },
       ],
